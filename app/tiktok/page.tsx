e334af293894eb0bd9661ad8ac5e12b93934daa7
User <user@pop-os.localdomain>
2024-03-26 12:04:55 +0700

    first commit

--- app/tiktok/page.tsx
+++ app/tiktok/page.tsx
@@ -2,8 +2,8 @@ import Tiktok from './tiktok'
 import config from "../../components/config.json";
 
 export const metadata = {
-  title: "TikTok Downloader Tanpa Watermark HD",
-  description: "TikTok Downloader Tanpa Watermark HD",
+  title: "TikTok Downloader - Without Watermark Full HD",
+  description: "TikTok Downloader - Without Watermark Full HD",
 }
 
 export default function Page(){
