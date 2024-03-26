7ce2df02fd9d223fa619cb2aed72f87ea287f861
User <user@pop-os.localdomain>
2024-03-26 12:16:54 +0700

    first commit

--- app/tiktok/page.tsx
+++ app/tiktok/page.tsx
@@ -4,6 +4,16 @@ import config from "../../components/config.json";
 export const metadata = {
   title: "TikTok Downloader - Without Watermark Full HD",
   description: "TikTok Downloader - Without Watermark Full HD",
+  canonical: config.url,
+  openGraph: {
+    url: config.url,
+    title: config.sitename,
+    description: config.desc,
+    images: [
+      { url: config.logo },
+    ],
+    siteName: config.sitename,
+  }
 }
 
 export default function Page(){
