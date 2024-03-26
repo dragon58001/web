6032dd1c75ea49a1a05fa728dab6a4e00878cafa
User <user@pop-os.localdomain>
2024-03-26 11:14:18 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,24 @@
 import { redirect } from "next/navigation";
 
 const HomePage = () => {
-  redirect('/tiktok')
+  // redirect('/tiktok')
   // const postMetadata = getPostMetadata();
   // const postPreviews = postMetadata.slice(0, 10).map((post) => (
   //   <PostPreview key={post.slug} {...post} />
   // ));
-    
-  // return (
-  //   <>
-  //   <div className="grid grid-cols-1 gap-4 md:grid-cols-2">{postPreviews}</div>
-  //   </>
-  // );
+
+  return (
+    <>
+    <h1 className="my-2 text-2xl font-bold text-center text-black">Download Video</h1>
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+      <div className="p-4">
+        <a href="/tiktok">
+          <h2 className="px-6 py-5 mx-10 text-2xl font-bold text-center text-white bg-black rounded-md hover:underline">TikTok</h2>
+        </a>
+      </div>
+    </div>
+    </>
+  );
 };
 
 export default HomePage;
